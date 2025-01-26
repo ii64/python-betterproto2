668a2a16bb315deb8e8ef9226c5f938f5e3bb213
Adrien <adrien.vannson@protonmail.com>
2025-01-26 23:27:41 +0100

    Test any to_dict (#63)
    * Test any to_dict
    
    * Add test

--- tests/test_any.py
+++ tests/test_any.py
@@ -11,3 +11,33 @@ def test_any() -> None:
     new_any = Any().parse(bytes(any))
 
     assert new_any.unpack() == person
+
+
+def test_any_to_dict() -> None:
+    from tests.output_betterproto.any import Person
+    from tests.output_betterproto.google.protobuf import Any
+
+    person = Person(first_name="John", last_name="Smith")
+
+    any = Any()
+
+    # TODO test with include defautl value
+    assert any.to_dict() == {"@type": ""}
+
+    # Pack an object inside
+    any.pack(person)
+
+    assert any.to_dict() == {
+        "@type": "type.googleapis.com/any.Person",
+        "firstName": "John",
+        "lastName": "Smith",
+    }
+
+    # Pack again in another Any
+    any2 = Any()
+    any2.pack(any)
+
+    assert any2.to_dict() == {
+        "@type": "type.googleapis.com/google.protobuf.Any",
+        "value": {"@type": "type.googleapis.com/any.Person", "firstName": "John", "lastName": "Smith"},
+    }
